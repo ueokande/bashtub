0caaf2ec02652f6a74d132c372a2e3a166c17a2a
Shin'ya Ueoka <ueokande@i-beam.org>
2015-11-03 11:21:08 +0900

    Add test for parameter option

---
+++ test/paramter_option_test.sh
@@ -0,0 +1,22 @@
+testcase_outputs_missing_operand() {
+  subject $0
+  assert_match 'missing' "$stderr"
+  assert_equal 1 $status
+}
+
+testcase_outputs_help() {
+  subject $0 --help
+  assert_match 'Usage' "$stdout"
+}
+
+testcase_output_version() {
+  subject $0 --version
+  assert_match 'bashtub' "$stdout"
+}
+
+testcase_output_unrecognized_option() {
+  subject $0 --sushi
+  assert_match 'unrecognized' "$stderr"
+  assert_match 'sushi' "$stderr"
+  assert_equal 1 $status
+}
